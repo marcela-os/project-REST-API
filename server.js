e145d79dda8e4fc3418149f0f7fd86982f224fad
Marcelina <m.oscislowska@gmail.com>
2020-12-10 16:44:14 +0100

    Fix server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/api', testimonialsRoutes); // add user routes to server
@@ -18,8 +18,8 @@ app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes); 
 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/public/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.use((req, res) => {
@@ -29,3 +29,4 @@ app.use((req, res) => {
 app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
+
